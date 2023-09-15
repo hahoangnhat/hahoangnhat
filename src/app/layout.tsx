20af764cc0ea426c1e9755dd123bde560a2b2ab0
hahoangnhat <hahoangnhat@sunday.local>
2023-09-15 21:15:17 +0700

    Init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Hà Hoàng Nhật',
+  description: 'This is my simple note during my learning to become a DevOps engineer.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
